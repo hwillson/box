51844dd5f04c927849403c761d83c14f9acac070
hwillson <hugh@octonary.com>
2015-08-26 07:07:03 -0400

    Updated browser policy to include https://thefeed.com.

--- server/config/policy.js
+++ server/config/policy.js
@@ -6,6 +6,7 @@ BrowserPolicy.framing.allowAll();
 var trusted = [
   'http://*.octonary.com',
   'http://*.thefeed.com',
+  'https://*.thefeed.com',
   'http://*.googleapis.com',
   'https://*.googleapis.com',
   'http://fonts.gstatic.com',
